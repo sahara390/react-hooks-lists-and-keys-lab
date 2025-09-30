2ebe3f01ba9e5b33def678469efca5e3ec413897
sahara <saharina123y@gmail.com>
2025-09-30 12:03:39 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,36 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 import ProjectList from "./ProjectList";
-import user from "../data/user";
+
 
 function App() {
+  const projects = [
+    {
+      id: 1,
+      name: "Portfolio Website",
+      about: "A personal portfolio site to showcase my work.",
+      technologies: ["React", "CSS", "JavaScript"]
+    },
+    {
+      id: 2,
+      name: "Todo App",
+      about: "A simple todo list application.",
+      technologies: ["HTML", "CSS", "JavaScript"]
+    },
+    {
+      id: 3,
+      name: "Blog Platform",
+      about: "A blog platform with user authentication.",
+      technologies: ["Node.js", "Express", "MongoDB"]
+    }
+  ];
+
   return (
     <div>
       <NavBar />
-      <Home username={user.name} city={user.city} color={user.color} />
-      <About bio={user.bio} links={user.links} />
-      <ProjectList projects={user.projects} />
+      <Home name="Sahara Ahmed" city="Nairobi" />
+      <About bio="Learning React step by step!" github="https://github.com/sahara" linkedin="https://linkedin.com/in/sahara" />
+      <ProjectList projects={projects} />
     </div>
   );
 }
